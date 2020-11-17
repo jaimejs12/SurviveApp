06c5aef0c5fab40a603a7ee03a5e803235129b47
JAIME JURADO SANCHEZ <Jaime.Jurado@alu.uclm.es>
2020-11-17 11:19:45 +0100

    Pantalla menú conseguir alimentos

---
+++ app/src/main/java/app/surviveapp/menu_conseguir_alimentos.java
@@ -0,0 +1,14 @@
+package app.surviveapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menu_conseguir_alimentos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_conseguir_alimentos);
+    }
+}
---
+++ app/src/main/java/app/surviveapp/que_comer.java
@@ -0,0 +1,14 @@
+package app.surviveapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class que_comer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_que_comer);
+    }
+}
