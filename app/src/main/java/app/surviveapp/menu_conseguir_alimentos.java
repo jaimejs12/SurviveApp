93aa4106479b3da91f8c2d9945fe635f35ba23d6
JAIME JURADO SANCHEZ <Jaime.Jurado@alu.uclm.es>
2020-11-18 18:44:17 +0100

    Página qué comer

--- app/src/main/java/app/surviveapp/menu_conseguir_alimentos.java
+++ app/src/main/java/app/surviveapp/menu_conseguir_alimentos.java
@@ -2,7 +2,9 @@ package app.surviveapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class menu_conseguir_alimentos extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class menu_conseguir_alimentos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_conseguir_alimentos);
     }
+
+    public void queComer(View view) {
+        Intent queComer = new Intent(this, que_comer.class);
+        startActivity(queComer);
+    }
+
 }
