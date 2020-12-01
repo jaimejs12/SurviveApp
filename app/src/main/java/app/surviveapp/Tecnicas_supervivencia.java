d572141805366f088dda443ac579dc0b6074ebe7
andreaortiz2 <73663129+andreaortiz2@users.noreply.github.com>
2020-12-01 14:20:36 +0100

    Artículo de cómo hacer un refugio

--- app/src/main/java/app/surviveapp/Tecnicas_supervivencia.java
+++ app/src/main/java/app/surviveapp/Tecnicas_supervivencia.java
@@ -1,6 +1,8 @@
 package app.surviveapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,4 +14,33 @@ public class Tecnicas_supervivencia extends AppCompatActivity {
         setContentView(R.layout.activity_tecnicas_supervivencia);
     }
 
+    public void PantallaRefugio(View view) {
+        Intent sigpantalla = new Intent(this, articulo_refugio.class);
+        startActivity(sigpantalla);
+    }
+
+    public void PantallaFuego(View view) {
+        Intent sigpantalla = new Intent(this, Tecnicas_supervivencia.class);
+        startActivity(sigpantalla);
+    }
+
+    public void PantallaAgua(View view) {
+        Intent sigpantalla = new Intent(this, Tecnicas_supervivencia.class);
+        startActivity(sigpantalla);
+    }
+
+    public void PantallaRescate(View view) {
+        Intent sigpantalla = new Intent(this, Tecnicas_supervivencia.class);
+        startActivity(sigpantalla);
+    }
+
+    public void PantallaFrio(View view) {
+        Intent sigpantalla = new Intent(this, Tecnicas_supervivencia.class);
+        startActivity(sigpantalla);
+    }
+
+    public void PantallaCalor(View view) {
+        Intent sigpantalla = new Intent(this, Tecnicas_supervivencia.class);
+        startActivity(sigpantalla);
+    }
 }
---
+++ app/src/main/java/app/surviveapp/articulo_refugio.java
@@ -0,0 +1,17 @@
+package app.surviveapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class articulo_refugio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tecnicas_supervivencia);
+    }
+
+}
