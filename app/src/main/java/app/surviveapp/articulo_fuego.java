4136822dbb5f633dcb8af94f149c7072a50bbf97
andreaortiz2 <73663129+andreaortiz2@users.noreply.github.com>
2020-12-02 14:34:28 +0100

    Artículo para obtener agua potable

---
+++ app/src/main/java/app/surviveapp/articulo_fuego.java
@@ -0,0 +1,14 @@
+package app.surviveapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class articulo_fuego extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_articulo_fuego);
+    }
+
+}
