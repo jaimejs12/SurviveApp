0f7b8d984e46e98796672da2055152a0865743a4
andreaortiz2 <73663129+andreaortiz2@users.noreply.github.com>
2020-12-13 17:38:13 +0100

    Herramientas y técnicas de supervivencia

--- app/src/main/java/app/surviveapp/guia_y_manual.java
+++ app/src/main/java/app/surviveapp/guia_y_manual.java
@@ -14,13 +14,38 @@ public class guia_y_manual extends AppCompatActivity {
         setContentView(R.layout.activity_guia_y_manual);
     }
 
-    public void aaa(View view) {
-        Intent sol = new Intent(this, ComoOrientarse.class);
+    public void volver(View view) {
+        Intent sol = new Intent(this, MainActivity.class);
         startActivity(sol);
     }
 
     public void PantallaTecnicas(View view) {
         Intent sigpantalla = new Intent(this, Tecnicas_supervivencia.class);
+        startActivity(sigpantalla);
+    }
+
+    public void PantallaOrientarse(View view) {
+        Intent sigpantalla = new Intent(this, ComoOrientarse.class);
+        startActivity(sigpantalla);
+    }
+
+    public void PantallaAlimentos(View view) {
+        Intent sigpantalla = new Intent(this, menu_conseguir_alimentos.class);
+        startActivity(sigpantalla);
+    }
+
+    public void PantallaKit(View view) {
+        Intent sigpantalla = new Intent(this, menu_conseguir_alimentos.class);
+        startActivity(sigpantalla); //CAMBIARRR
+    }
+
+    public void PantallaAccidentes(View view) {
+        Intent sigpantalla = new Intent(this, AfrontarAccidentes.class);
+        startActivity(sigpantalla);
+    }
+
+    public void PantallaHeridas(View view) {
+        Intent sigpantalla = new Intent(this, curar_heridas.class);
         startActivity(sigpantalla);
     }
 }
